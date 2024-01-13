675e842820d36391ee5b95799409759055b1be33
Koudocko <tyler@pcquest.ca>
2024-01-12 20:45:37 -0500

    Added assignment 1 and lab 1 zips.

--- Labs/wehr6772_l01/src/t04.py
+++ Labs/wehr6772_l01/src/t04.py
@@ -9,9 +9,6 @@ __updated__ = "2024-01-01"
 -------------------------------------------------------
 """
 
-import sys,os
-sys.path.append(os.path.realpath('../../../wehr6772_data_structures/src'))
-
 from Food import Food
 from Food_utilities import *
 
